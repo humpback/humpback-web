6642f0f2f290f59c4fbd38a9730575d31a337824
lon.yang <634377375@qq.com>
2017-01-23 15:32:54 +0800

    improve gulp build process

--- src/client/main.ts
+++ src/client/main.ts
@@ -2,10 +2,13 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 
+declare let lightReload: any;
+
 if (process.env.ENV === 'production') {
   enableProdMode();
 } else {
-  require('./static/js/auto-reload.js');
+  require('light-reload/client');
+  lightReload.init(9107, { maxReconnectCount: 10 });
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule);
