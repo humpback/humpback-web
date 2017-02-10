f4b87b25ef32573638762f829d720159b3c479fd
lon.yang <634377375@qq.com>
2017-02-10 13:15:13 +0800

    fix a bug when init system

--- src/server/db/dbFactory.js
+++ src/server/db/dbFactory.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 const Datastore = require('nedb');
 const config = require('./../config');
 
@@ -17,11 +18,15 @@ class DBFactory {
   }
 
   connectDB() {
+    let dbFilesPath = path.join(__dirname, `./../dbFiles`);
+    if (!fs.existsSync(dbFilesPath)) {
+      fs.mkdirSync(dbFilesPath);
+    }
     for (let key in config.dbConfigs) {
       let item = config.dbConfigs[key];
       if (item.ignoreLoad) continue;
       let option = {
-        filename: path.join(__dirname, `./../dbFiles/${item.name}.db`),
+        filename: path.join(dbFilesPath, `${item.name}.db`),
         autoload: true
       };
       if (item.ttl) {
