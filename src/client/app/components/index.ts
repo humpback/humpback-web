aef9858719466daff82cbd091736a45d6438f3eb
lon <634377375@qq.com>
2017-03-30 19:33:24 +0800

    Optimized code

--- src/client/app/components/index.ts
+++ src/client/app/components/index.ts
@@ -1,14 +1,8 @@
-import { HeaderComponent } from './header';
-import { SideBarComponent } from './sidebar';
-import { ModalComponent, ModalHeaderComponent, ModalFooterComponent } from './modal';
-import { PaginationComponent } from './pagination';
-import { TagsInputComponent } from './tags-input';
-
-export * from './header';
-export * from './sidebar';
-export * from './modal';
-export * from './pagination';
-export * from './tags-input';
+import { HeaderComponent } from './header/header.component';
+import { SideBarComponent } from './sidebar/sidebar.component';
+import { ModalComponent, ModalHeaderComponent, ModalFooterComponent } from './modal/modal.cpmponent';
+import { PaginationComponent } from './pagination/pagination.component';
+import { TagsInputComponent } from './tags-input/tags-input.component';
 
 let Components: Array<any> = [
   HeaderComponent,
@@ -19,5 +13,15 @@ let Components: Array<any> = [
   PaginationComponent,
   TagsInputComponent
 ]
+
+export {
+  HeaderComponent,
+  SideBarComponent,
+  ModalComponent,
+  ModalHeaderComponent,
+  ModalFooterComponent,
+  PaginationComponent,
+  TagsInputComponent
+}
 
 export const COMPONENTS = Components;
